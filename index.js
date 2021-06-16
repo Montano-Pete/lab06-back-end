93c7464d2dba3d8959b68ca1480f5c5cd9e9b083
Peter <pete225848@gmail.com>
2021-06-16 12:55:10 -0700

    test passes

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-const express = require('express')
-const fruits = require('./data.js')
-const app = express()
+const app = require('./server.js');
 const port = process.env.PORT || 3001;
 
-app.get('/fruits', (req, res) => {
-    
-    res.json(fruits);
-});
-
-app.get('/fruits/:id', (req, res) => {
-    const id = Number(req.params.id);
-
-    const leFruits = fruits.find((singleFruit) => singleFruit.id === id);
-
-    res.json(leFruits);
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
